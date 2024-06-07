7f8c585db89937bfa6a1598de6c8493c29708d10
Bryan Moy <dev@bryanmoy.com>
2024-06-07 23:48:41 +0200

    refactor(git): Create Module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,6 +12,7 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
+    outputs.homeManagerModules.git
     outputs.homeManagerModules.foot
     outputs.homeManagerModules.hyprland
 
@@ -71,20 +72,20 @@
   };
 
   # Git
-  programs.git = {
-    enable = true;
-    userName = "Bryan Moy";
-    userEmail = "dev@bryanmoy.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvMq/Zpl7z9G5EOv9lfI7XK+U4SnNSq9PMGU6Kv7SaC";
-      commit.gpgsign = true;
-      tag.gpgsign = true;
-      gpg.format = "ssh";
-      gpg.ssh.program = "op-ssh-sign";
-      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-    };
-  };
+  # programs.git = {
+  #   enable = true;
+  #   userName = "Bryan Moy";
+  #   userEmail = "dev@bryanmoy.com";
+  #   extraConfig = {
+  #     init.defaultBranch = "main";
+  #     user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvMq/Zpl7z9G5EOv9lfI7XK+U4SnNSq9PMGU6Kv7SaC";
+  #     commit.gpgsign = true;
+  #     tag.gpgsign = true;
+  #     gpg.format = "ssh";
+  #     gpg.ssh.program = "op-ssh-sign";
+  #     gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+  #   };
+  # };
 
   # Shell
   programs.starship.enable = true;
@@ -167,6 +168,7 @@
   '';
 
   modules = {
+    git.enable = true;
     foot.enable = true;
     hyprland.enable = true;
   };
