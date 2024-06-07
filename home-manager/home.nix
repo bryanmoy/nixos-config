b041f7ee75ac30fd50fb08e39eca2bb3a72b08de
Bryan Moy <dev@bryanmoy.com>
2024-06-07 23:49:17 +0200

    refactor(git): Create Module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -71,22 +71,6 @@
     '';
   };
 
-  # Git
-  # programs.git = {
-  #   enable = true;
-  #   userName = "Bryan Moy";
-  #   userEmail = "dev@bryanmoy.com";
-  #   extraConfig = {
-  #     init.defaultBranch = "main";
-  #     user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvMq/Zpl7z9G5EOv9lfI7XK+U4SnNSq9PMGU6Kv7SaC";
-  #     commit.gpgsign = true;
-  #     tag.gpgsign = true;
-  #     gpg.format = "ssh";
-  #     gpg.ssh.program = "op-ssh-sign";
-  #     gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-  #   };
-  # };
-
   # Shell
   programs.starship.enable = true;
   programs.starship.settings = {}; # TODO: https://starship.rs/config/
