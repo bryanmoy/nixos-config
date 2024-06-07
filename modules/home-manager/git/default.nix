612b9a0838c388fd32afa1f75cf005c91beee12b
Bryan Moy <dev@bryanmoy.com>
2024-06-07 23:55:46 +0200

    style(git): Formatting

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -6,19 +6,19 @@ let cfg = config.modules.git;
 in {
     options.modules.git = { enable = mkEnableOption "git"; };
     config = mkIf cfg.enable {
-          programs.git = {
-          enable = true;
-          userName = "Bryan Moy";
-          userEmail = "dev@bryanmoy.com";
-          extraConfig = {
-            init.defaultBranch = "main";
-            user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvMq/Zpl7z9G5EOv9lfI7XK+U4SnNSq9PMGU6Kv7SaC";
-            commit.gpgsign = true;
-            tag.gpgsign = true;
-            gpg.format = "ssh";
-            gpg.ssh.program = "op-ssh-sign";
-            gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-          };
+        programs.git = {
+            enable = true;
+            userName = "Bryan Moy";
+            userEmail = "dev@bryanmoy.com";
+            extraConfig = {
+              init.defaultBranch = "main";
+              user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvMq/Zpl7z9G5EOv9lfI7XK+U4SnNSq9PMGU6Kv7SaC";
+              commit.gpgsign = true;
+              tag.gpgsign = true;
+              gpg.format = "ssh";
+              gpg.ssh.program = "op-ssh-sign";
+              gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+            };
         };
     };
 }
