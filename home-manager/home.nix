0940624164112593ac36f389c7ef83d0df841acd
Bryan Moy <dev@bryanmoy.com>
2024-06-07 23:42:34 +0200

    chore: Cleanup

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -60,35 +60,6 @@
   # Enable home-manager
   programs.home-manager.enable = true;
 
-  # # Hyprland
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-  #   settings = {
-  #     "$mod" = "SUPER";
-  #     bind =
-  #       [
-  #         "$mod, F, exec, firefox"
-  #         ", Print, exec, grimblast copy area"
-  #       ]
-  #       ++ (
-  #         # workspaces
-  #         # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-  #         builtins.concatLists (builtins.genList (
-  #             x: let
-  #               ws = let
-  #                 c = (x + 1) / 10;
-  #               in
-  #                 builtins.toString (x + 1 - (c * 10));
-  #             in [
-  #               "$mod, ${ws}, workspace, ${toString (x + 1)}"
-  #               "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-  #             ]
-  #           )
-  #           10)
-  #       );
-  #   };
-  # };
-
   # SSH
   programs.ssh = {
     enable = true;
