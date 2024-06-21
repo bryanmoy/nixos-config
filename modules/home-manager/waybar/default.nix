35127ea660aff5dbcde8f681bb74c60c37cf207b
Bryan Moy <dev@bryanmoy.com>
2024-06-21 21:44:55 +0200

    chore(waybar): Update config

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -13,29 +13,30 @@ in {
       enable = true;
       settings = {
         mainBar = {
+          ##################
+          ### BAR CONFIG ###
+          ################EE
+
+          # See https://github.com/Alexays/Waybar/wiki/Configuration#bar-config
           layer = "top";
-          position = "top";
-          height = 30;
-          # output = [
-          #   "eDP-1"
-          #   "HDMI-A-1"
-          # ];
-          # modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
-          # modules-center = [ "sway/window" "custom/hello-from-waybar" ];
-          # modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
-      
-          # "sway/workspaces" = {
-          #   disable-scroll = true;
-          #   all-outputs = true;
-          # };
-          # "custom/hello-from-waybar" = {
-          #   format = "hello {}";
-          #   max-length = 40;
-          #   interval = "once";
-          #   exec = pkgs.writeShellScript "hello-from-waybar" ''
-          #     echo "from within waybar"
-          #   '';
-          # };
+          modules-left = [
+            # "sway/workspaces"
+            # "sway/mode"
+          ];
+          modules-center = [
+            # "sway/window"
+          ];
+          modules-right = [
+            "battery"
+            "clock"
+          ];
+          battery = {
+            format = "{capacity}% {icon}";
+            format-icons = ["" "" "" "" ""];
+          };
+          clock = {
+            format-alt = "{:%a, %d. %b  %H:%M}";
+          };
         };
       };
     };
